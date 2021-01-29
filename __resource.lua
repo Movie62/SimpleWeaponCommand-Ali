51797c77ab5a137c0e17eaa0719cf2912d5feb82
Ali <75635207+AliProduction@users.noreply.github.com>
2021-01-29 20:53:28 +0100

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,19 @@
+-- DE --
+--[[ Author: Ali         
+ Dies ist ein Waffen geben script damit mann nicht immer ein Roman schreiben muss um eine Waffe sich zu geben
+ Also eine vereinfachte Form sich Waffen zu geben
+--]]
+
+-- EN --
+--[[
+ This is a gun giving script so you don't always have to write a novel to give yourself a gun
+ Also a simplified way of giving yourself weapons
+--]]
+
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+   "functions.lua",
+   "guns-c.lua"
+} 
